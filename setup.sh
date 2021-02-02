4e488ed300a4584da17cc2b62d53e69b5b2e6a62
Santiago Hurtado <shurtado@rosen-group.com>
2021-02-02 11:41:36 +0100

    feat: Add script to download intel linpack

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl https://software.intel.com/content/dam/develop/external/us/en/documents/l_mklb_p_2020.2.001.tgz -o l_mklb_p_2020.2.001.tgz
+tar xfz l_mklb_p_2020.2.001.tgz
+cd l_mklb_p_2020.2.001/benchmarks_2020/linux/mkl/benchmarks/linpack
