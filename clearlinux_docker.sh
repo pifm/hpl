249a94f7480c6a17eadd27431914ff181b1fa41b
Santiago Hurtado <shurtado@rosen-group.com>
2021-02-03 11:26:36 +0100

    fix: docker service needs to be started

--- clearlinux_docker.sh
+++ clearlinux_docker.sh
@@ -3,5 +3,7 @@ sudo swupd bundle-add containers-basic
 sudo usermod -aG docker ${USER}
 curl https://raw.githubusercontent.com/pifm/hpl/main/ubuntu.dockerfile -o ubuntu.dockerfile
 curl https://raw.githubusercontent.com/pifm/hpl/main/clearlinux.dockerfile -o clearlinux.dockerfile
+sudo systemctl start docker
 sudo docker build . -f ubuntu.dockerfile -t hpl:ubuntu
 sudo docker build . -f clearlinux.dockerfile -t hpl:clearlinux
+sudo systemctl enable docker
